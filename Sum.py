14b2fd5fc604e2de52475012a3d0729b2346014f
coder00420 <nerograf.vs@gmail.com>
2025-10-03 23:01:12 +0300

    исправленны недочёты программы

--- Sum.py
+++ Sum.py
@@ -1,7 +1,12 @@
 number = int(open("INPUT.TXT", "r").read())
+is_negated = False
+if number < 0:
+    -number
+    is_negated = True
 sum_ = 0
 i = 1
 while i <= number:
     sum_ += i
     i += 1
+if is_negated == True: -number
 open("OUTPUT.TXT", 'w').write(str(sum_))
