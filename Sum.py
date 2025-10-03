739065701c103bf42b1a06eea5b391b651692593
coder00420 <nerograf.vs@gmail.com>
2025-10-03 23:17:01 +0300

    исправлено неправильное применение операторов

--- Sum.py
+++ Sum.py
@@ -1,12 +1,12 @@
 number = int(open("INPUT.TXT", "r").read())
 is_negated = False
 if number < 0:
-    -number
+    number = -number
     is_negated = True
 sum_ = 0
 i = 1
 while i <= number:
     sum_ += i
     i += 1
-if is_negated == True: -number
+if is_negated == True: sum_ = -sum_ + 1
 open("OUTPUT.TXT", 'w').write(str(sum_))
